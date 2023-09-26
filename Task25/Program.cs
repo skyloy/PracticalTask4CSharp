ce3b13a2b4623895a9fb3e82b26a582a27a5b862
«Ilya» <krutoipacan@ilonmask.com>
2023-09-26 11:57:09 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+using System.Reflection.Metadata.Ecma335;
+
+int Power(int num1, int num2)
+{
+    int res = num1;
+        for (int i = 0; i < num2 - 1; i++)
+    {
+        res *= num1;
+    }
+    return res;
+}
+Console.WriteLine("Please enter the numbers");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numB < 0)
+
+    Console.WriteLine("this is not a natural number");
+else
+    Console.WriteLine(Power(numA, numB));
