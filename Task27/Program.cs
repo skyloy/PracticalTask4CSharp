7f7173edd783a1150ed9e932a7254bb4d834f2d5
«Ilya» <krutoipacan@ilonmask.com>
2023-09-26 12:26:49 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+void DigitsSum(int num)
+{
+    int res = 0;
+    int digits = 0;
+    for (int i = num; i > 0; i/=10)
+    {
+        digits = num % 10;
+        res += digits;
+        num /= 10;
+    }
+    Console.WriteLine(res);
+}
+Console.WriteLine("Please enter the number:");
+int numb = Convert.ToInt32(Console.ReadLine());
+DigitsSum(numb);
