845a18794b3e4e4596e9f4e721f366185248c1bf
«Ilya» <krutoipacan@ilonmask.com>
2023-09-26 12:53:26 +0300

    add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("]");
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
